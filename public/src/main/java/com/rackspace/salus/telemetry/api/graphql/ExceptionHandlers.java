895eb4ca3f457e447c499f46fd43c1265e8dbe83
Geoff Bourne <geoff.bourne@rackspace.com>
2018-12-06 14:09:38 -0600

    Documented the ExceptionHandlers class

--- public/src/main/java/com/rackspace/salus/telemetry/api/graphql/ExceptionHandlers.java
+++ public/src/main/java/com/rackspace/salus/telemetry/api/graphql/ExceptionHandlers.java
@@ -5,11 +5,18 @@ import graphql.GraphQLException;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+/**
+ * GraphQL will scan for {@link ExceptionHandler} annotated methods that return a {@link GraphQLException}
+ * in order to translate application specific annotations, matched by the method argument type,
+ * into one that GraphQL can specifically convey back to the caller of the API.
+ */
 @Component
 public class ExceptionHandlers {
 
   @ExceptionHandler
   public GraphQLException handleNotFound(NotFoundException e) {
+    // GraphQL will extract things like the message from the given exception, so it might turn
+    // out this method can be broadened to catch any type of exception.
     return new GraphQLException("Not found", e);
   }
 
