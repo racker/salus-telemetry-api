44c48d042fe68f3e35fb8c2981702eb445138c11
James Buchan <james.buchan@rackspace.com>
2018-12-10 14:45:36 -0700

    Add get* resources api

--- public/src/main/java/com/rackspace/salus/telemetry/api/graphql/Converters.java
+++ public/src/main/java/com/rackspace/salus/telemetry/api/graphql/Converters.java
@@ -3,12 +3,17 @@ package com.rackspace.salus.telemetry.api.graphql;
 import com.rackspace.salus.telemetry.api.model.AgentConfigInput;
 import com.rackspace.salus.telemetry.api.model.AgentConfigResponse;
 import com.rackspace.salus.telemetry.api.model.Label;
+import com.rackspace.salus.telemetry.api.model.ResourceResponse;
 import com.rackspace.salus.telemetry.model.AgentConfig;
+import com.rackspace.salus.telemetry.model.ResourceInfo;
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+@Slf4j
 public class Converters {
 
   static List<Label> convertToLabelList(Map<String, String> labels) {
@@ -51,5 +56,21 @@ public class Converters {
         .setContent(config.getContent())
         .setSelectorScope(config.getSelectorScope())
         .setLabels(convertToLabelMap(config.getLabels()));
+  }
+
+  static ResourceResponse convertToResponse(ResourceInfo resourceInfo) {
+    return new ResourceResponse()
+            .setIdentifier(resourceInfo.getIdentifier())
+            .setIdentifierValue(resourceInfo.getIdentifierValue())
+            .setTenantId(resourceInfo.getTenantId())
+            .setEnvoyId(resourceInfo.getEnvoyId())
+            .setLabels(convertToLabelList(resourceInfo.getLabels()));
+  }
+
+  static List<ResourceResponse> convertToResourceResponse(List<ResourceInfo> resourceInfos) {
+    return resourceInfos.stream()
+            .map(Converters::convertToResponse
+            )
+            .collect(Collectors.toList());
   }
 }
