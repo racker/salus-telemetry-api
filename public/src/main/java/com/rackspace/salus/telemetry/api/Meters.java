07e4a35cc0309e561004b51604964097a82ace5f
James Buchan <james.buchan@rackspace.com>
2018-12-10 14:45:50 -0700

    Add delete resources api

--- public/src/main/java/com/rackspace/salus/telemetry/api/Meters.java
+++ public/src/main/java/com/rackspace/salus/telemetry/api/Meters.java
@@ -3,4 +3,5 @@ package com.rackspace.salus.telemetry.api;
 public class Meters {
 
   public static final String AGENT_CONFIGS_TYPE = "agentConfigs";
+  public static final String RESOURCES_TYPE = "resources";
 }
