3b158429af997803dc9fa39e62c5e18be00e8750
Geoff Bourne <geoff.bourne@rackspace.com>
2019-03-20 11:57:51 -0500

    Remove GraphQL SPQR dependency

--- public/src/main/java/com/rackspace/salus/telemetry/api/model/AgentInstallation.java
+++ public/src/main/java/com/rackspace/salus/telemetry/api/model/AgentInstallation.java
@@ -1,19 +1,13 @@
 package com.rackspace.salus.telemetry.api.model;
 
 import com.rackspace.salus.telemetry.model.Label;
-import io.leangen.graphql.annotations.GraphQLIgnore;
 import java.util.List;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 @Data
 public class AgentInstallation {
   String id;
 
-  // this will be used to stitch together the final GraphQL response object
-  @Getter(onMethod = @__({@GraphQLIgnore}))
-  @Setter(onMethod = @__({@GraphQLIgnore}))
   String agentReleaseId;
 
   List<Label> matchingLabels;
