25c22c399bd3f0520f7c583f81bc218bda00adde
James Buchan <james.buchan@rackspace.com>
2019-05-14 12:18:46 -0600

    Add Public Zone API

---
+++ public/src/main/java/com/rackspace/salus/telemetry/api/web/ZonesController.java
@@ -0,0 +1,99 @@
+/*
+ * Copyright 2019 Rackspace US, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.rackspace.salus.telemetry.api.web;
+
+import com.rackspace.salus.telemetry.api.config.ServicesProperties;
+import com.rackspace.salus.telemetry.api.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.mvc.ProxyExchange;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@RestController
+@RequestMapping("/api")
+@SuppressWarnings("Duplicates") // due to repetitive proxy setup/calls
+public class ZonesController {
+  private final UserService userService;
+  private final ServicesProperties servicesProperties;
+
+  @Autowired
+  public ZonesController(UserService userService, ServicesProperties servicesProperties) {
+    this.userService = userService;
+    this.servicesProperties = servicesProperties;
+  }
+
+  @GetMapping("/zones")
+  public ResponseEntity<?> getAll(ProxyExchange<?> proxy,
+                                  @RequestParam(defaultValue = "100") int size,
+                                  @RequestParam(defaultValue = "0") int page) {
+    final String tenantId = userService.currentTenantId();
+
+    final String backendUri = UriComponentsBuilder
+        .fromUriString(servicesProperties.getMonitorManagementUrl())
+        .path("/api/tenant/{tenantId}/zones")
+        .queryParam("size", size)
+        .queryParam("page", page)
+        .build(tenantId)
+        .toString();
+
+    return proxy.uri(backendUri).get();
+  }
+
+  @PostMapping("/zones")
+  public ResponseEntity<?> create(ProxyExchange<?> proxy) {
+    final String tenantId = userService.currentTenantId();
+
+    final String backendUri = UriComponentsBuilder
+        .fromUriString(servicesProperties.getMonitorManagementUrl())
+        .path("/api/tenant/{tenantId}/zones")
+        .build(tenantId)
+        .toString();
+
+    return proxy.uri(backendUri)
+        .post();
+  }
+
+  @PutMapping("/zones/{name}")
+  public ResponseEntity<?> update(ProxyExchange<?> proxy,
+                                  @PathVariable String name) {
+    final String tenantId = userService.currentTenantId();
+
+    final String backendUri = UriComponentsBuilder
+        .fromUriString(servicesProperties.getMonitorManagementUrl())
+        .path("/api/tenant/{tenantId}/zones/{name}")
+        .build(tenantId, name)
+        .toString();
+
+    return proxy.uri(backendUri)
+        .put();
+  }
+
+  @DeleteMapping("/zones/{name}")
+  public ResponseEntity<?> delete(ProxyExchange<?> proxy,
+                                  @PathVariable String name) {
+    final String tenantId = userService.currentTenantId();
+
+    final String backendUri = UriComponentsBuilder
+        .fromUriString(servicesProperties.getMonitorManagementUrl())
+        .path("/api/tenant/{tenantId}/zones/{name}")
+        .build(tenantId, name)
+        .toString();
+
+    return proxy.uri(backendUri).delete();
+  }
+}
