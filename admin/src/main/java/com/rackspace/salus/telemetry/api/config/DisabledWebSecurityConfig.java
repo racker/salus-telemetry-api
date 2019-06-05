090e1b0f6aecf2528d9d0c75ff4c398f6edb05ae
Geoff Bourne <itzgeoff@gmail.com>
2019-06-05 16:24:04 -0500

    Disabled security config doesn't need to exclude 'secured' profile

--- admin/src/main/java/com/rackspace/salus/telemetry/api/config/DisabledWebSecurityConfig.java
+++ admin/src/main/java/com/rackspace/salus/telemetry/api/config/DisabledWebSecurityConfig.java
@@ -22,7 +22,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-@Profile("!secured & !proxied-auth")
+@Profile("!proxied-auth")
 public class DisabledWebSecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
