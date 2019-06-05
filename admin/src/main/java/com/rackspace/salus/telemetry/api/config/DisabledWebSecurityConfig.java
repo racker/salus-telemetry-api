afc46f16b7a99dfbfbe5bdca96ceaede1b362b43
Geoff Bourne <geoff.bourne@rackspace.com>
2019-06-05 16:49:49 -0500

    Remove the direct use of SAML authentication since proxy is now used (#33)
    * Remove the direct use of SAML authentication since proxy is now used
    
    * Use 'secured' as the Spring profile name to unify with public api

--- admin/src/main/java/com/rackspace/salus/telemetry/api/config/DisabledWebSecurityConfig.java
+++ admin/src/main/java/com/rackspace/salus/telemetry/api/config/DisabledWebSecurityConfig.java
@@ -22,7 +22,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-@Profile("!secured & !proxied-auth")
+@Profile("!secured")
 public class DisabledWebSecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
