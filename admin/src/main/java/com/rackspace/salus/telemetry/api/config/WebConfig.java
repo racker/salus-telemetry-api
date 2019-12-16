4eca4e18351d79e2b1743b4e9b66e07aaacf908f
Geoff Bourne <geoff.bourne@rackspace.com>
2019-12-16 13:40:06 -0600

    Disable zipkin by default and log registration of RequestLogging (#66)

--- admin/src/main/java/com/rackspace/salus/telemetry/api/config/WebConfig.java
+++ admin/src/main/java/com/rackspace/salus/telemetry/api/config/WebConfig.java
@@ -17,15 +17,18 @@
 package com.rackspace.salus.telemetry.api.config;
 
 import com.rackspace.salus.common.web.RequestLogging;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@Slf4j
 public class WebConfig implements WebMvcConfigurer {
 
   @Override
   public void addInterceptors(InterceptorRegistry registry) {
+    log.debug("Enabling RequestLogging interceptor");
     registry.addInterceptor(new RequestLogging());
   }
 }
