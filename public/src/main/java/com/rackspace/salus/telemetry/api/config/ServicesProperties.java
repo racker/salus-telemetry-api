be90492aae8118e99de61137732699d8fad0c618
Geoff Bourne <geoff.bourne@rackspace.com>
2020-04-01 09:24:04 -0500

    Added envoy tokens endpoint

--- public/src/main/java/com/rackspace/salus/telemetry/api/config/ServicesProperties.java
+++ public/src/main/java/com/rackspace/salus/telemetry/api/config/ServicesProperties.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 Rackspace US, Inc.
+ * Copyright 2020 Rackspace US, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -33,4 +33,6 @@ public class ServicesProperties {
   String eventManagementUrl = "http://event-management:8080";
   @NotEmpty
   String agentCatalogManagementUrl = "http://agent-catalog-management:8080";
+  @NotEmpty
+  String authServiceUrl = "http://authserv:8082";
 }
