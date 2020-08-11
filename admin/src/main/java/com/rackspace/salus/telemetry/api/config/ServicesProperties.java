3dc61e5058e3af0894b6bbc014b069ccf9bc7003
Adam Martinek <harageth@users.noreply.github.com>
2020-08-11 11:37:18 -0700

    Adam/delete tenant (#96)
    * Added CrossServiceController to proxy through the request to delete everything for a tenant
    
    * Made sure the deletions are transactional. Updated Cross Service controller for correct URI paths.
    
    * Created response body from list of bodies from requests.
    
    * Set default values and allow for sending events on deleteAll. Also allow not sending events.
    
    * stopped applying the headers every request and checked the bodies of each response as we got them.
    
    * Added flag for keeping the tenant metadata but deleting everything else for the account
    
    * updated flag

--- admin/src/main/java/com/rackspace/salus/telemetry/api/config/ServicesProperties.java
+++ admin/src/main/java/com/rackspace/salus/telemetry/api/config/ServicesProperties.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 Rackspace US, Inc.
+ * Copyright 2020 Rackspace US, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,6 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
  */
 
 package com.rackspace.salus.telemetry.api.config;
@@ -40,4 +41,6 @@ public class ServicesProperties {
   String agentCatalogManagementUrl = "http://agent-catalog-management:8080";
   @NotBlank
   String policyManagementUrl = "http://policy-management:8080";
+  @NotEmpty
+  String authServiceUrl = "http://authserv:8082";
 }
---
+++ admin/src/main/java/com/rackspace/salus/telemetry/api/web/CrossServiceController.java
@@ -0,0 +1,177 @@
+/*
+ * Copyright 2020 Rackspace US, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.rackspace.salus.telemetry.api.web;
+
+import com.rackspace.salus.common.util.ApiUtils;
+import com.rackspace.salus.telemetry.api.config.ServicesProperties;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import org.eclipse.jetty.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.mvc.ProxyExchange;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@RestController
+@RequestMapping("/api")
+public class CrossServiceController {
+
+  private final ServicesProperties servicesProperties;
+
+  @Autowired
+  public CrossServiceController(ServicesProperties servicesProperties) {
+    this.servicesProperties = servicesProperties;
+
+  }
+
+  @DeleteMapping("/{tenantId}")
+  public ResponseEntity<?> deleteAll(ProxyExchange<?> proxy,
+      @RequestHeader HttpHeaders headers,
+      @RequestParam MultiValueMap<String,String> queryParams,
+      @PathVariable String tenantId,
+      @RequestParam(defaultValue = "true") boolean removeTenantMetadata) {
+    queryParams.add("sendEvents", "false");
+    List bodies = new LinkedList();
+
+    // delete agent installs
+    final String agentCatalogManagementURI = UriComponentsBuilder
+        .fromUriString(servicesProperties.getAgentCatalogManagementUrl())
+        .path("/api/admin/tenant/{tenantId}/agent-installs")
+        .queryParams(queryParams)
+        .buildAndExpand(tenantId)
+        .toUriString();
+
+    ApiUtils.applyRequiredHeaders(proxy, headers);
+
+    ResponseEntity<?> agentInstalls = proxy.uri(agentCatalogManagementURI).delete();
+    if(agentInstalls.getStatusCode().isError()) {
+      bodies.add(agentInstalls.getBody());
+    }
+
+    // delete envoy tokens
+    final String authURI = UriComponentsBuilder
+        .fromUriString(servicesProperties.getAuthServiceUrl())
+        .path("/api/admin/tenant/{tenantId}/envoy-tokens")
+        .queryParams(queryParams)
+        .buildAndExpand(tenantId)
+        .toUriString();
+
+    ApiUtils.applyRequiredHeaders(proxy, headers);
+
+    ResponseEntity<?> envoyTokens = proxy.uri(authURI).delete();
+
+    if(envoyTokens.getStatusCode().isError()) {
+      bodies.add(envoyTokens.getBody());
+    }
+
+    // delete event engine tasks
+    final String eventEngineURI = UriComponentsBuilder
+        .fromUriString(servicesProperties.getEventManagementUrl())
+        .path("/api/admin/tenant/{tenantId}/tasks")
+        .queryParams(queryParams)
+        .buildAndExpand(tenantId)
+        .toUriString();
+
+    ResponseEntity<?> eventResponse = proxy.uri(eventEngineURI).delete();
+
+    if(eventResponse.getStatusCode().isError()) {
+      bodies.add(eventResponse.getBody());
+    }
+
+    // Delete from resources first
+    final String resourcesURI = UriComponentsBuilder
+        .fromUriString(servicesProperties.getResourceManagementUrl())
+        .path("/api/admin/tenant/{tenantId}/resources")
+        .queryParams(queryParams)
+        .buildAndExpand(tenantId)
+        .toUriString();
+
+    ResponseEntity<?> resource = proxy.uri(resourcesURI).delete();
+
+    if(resource.getStatusCode().isError()) {
+      bodies.add(resource.getBody());
+    }
+
+    // Delete monitors
+    final String monitorsURI = UriComponentsBuilder
+        .fromUriString(servicesProperties.getMonitorManagementUrl())
+        .path("/api/admin/tenant/{tenantId}/monitors")
+        .queryParams(queryParams)
+        .buildAndExpand(tenantId)
+        .toUriString();
+
+    ResponseEntity<?> monitor = proxy.uri(monitorsURI).delete();
+
+    if(monitor.getStatusCode().isError()) {
+      bodies.add(monitor.getBody());
+    }
+
+    // delete tenant metadata
+    if(removeTenantMetadata) {
+      final String tenantMetadataURI = UriComponentsBuilder
+          .fromUriString(servicesProperties.getPolicyManagementUrl())
+          .path("/api/admin/tenant-metadata/{tenantId}")
+          .queryParams(queryParams)
+          .buildAndExpand(tenantId)
+          .toUriString();
+
+      ResponseEntity<?> tenantMetadata = proxy.uri(tenantMetadataURI).delete();
+
+      if (tenantMetadata.getStatusCode().isError()) {
+        bodies.add(tenantMetadata.getBody());
+      }
+    }
+
+    // delete zones
+    final String zonesURI = UriComponentsBuilder
+        .fromUriString(servicesProperties.getMonitorManagementUrl())
+        .path("/api/admin/tenant/{tenantId}/zones")
+        .queryParams(queryParams)
+        .buildAndExpand(tenantId)
+        .toUriString();
+
+    ResponseEntity<?> zone = proxy.uri(zonesURI).delete();
+
+    if(zone.getStatusCode().isError()) {
+      bodies.add(zone.getBody());
+    }
+
+    if(!bodies.isEmpty()) {
+      LinkedHashMap responseBody = new LinkedHashMap();
+
+      responseBody.put("messages", bodies);
+
+      return ResponseEntity.status(HttpStatus.CONFLICT_409).headers(headers).body(responseBody);
+    }
+
+    return ResponseEntity.status(HttpStatus.NO_CONTENT_204).headers(headers).build();
+  }
+
+
+
+
+}
